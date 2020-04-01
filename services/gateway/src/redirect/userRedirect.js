954b88bf26f198d380b76ede1e191779593237a6
florian <florianduport@gmail.com>
2020-04-01 16:17:56 +0200

    authenticate route through gateway

--- services/gateway/src/redirect/userRedirect.js
+++ services/gateway/src/redirect/userRedirect.js
@@ -9,6 +9,7 @@ class UserRedirect extends RedirectBase{
       //set routes to this.apiRoutes here
       var parent = this;
       this.apiRoutes.get('/users', function(req, res){ parent.get(req, res); });
+      this.apiRoutes.post('/authenticate', function(req, res){ parent.post(req, res); });
 
       this.setRouter(app);
   }
